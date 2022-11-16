6750feb04cbbda0f48abf468c57843dfcbf31f82
ba2dzc <batu.duzcu@gmail.com>
2022-11-16 15:45:25 +0300

    first commit

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,16 +3,24 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { NavbarComponent } from './navbar/navbar.component';
+import { MainComponent } from './main/main.component';
+import { SortSelectService } from './services/sortSelect.service';
 
 @NgModule({
-  declarations: [
-    AppComponent
-  ],
+  declarations: [				
+    AppComponent,
+      NavbarComponent,
+      MainComponent,
+      MainComponent,
+      NavbarComponent
+   ],
   imports: [
     BrowserModule,
     AppRoutingModule
   ],
-  providers: [],
+  providers: [SortSelectService],
   bootstrap: [AppComponent]
 })
 export class AppModule { }
+
